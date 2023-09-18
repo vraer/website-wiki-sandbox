ac58f495b6334ed71673eb0e1fe6e39920070aa3
Vanessa Rodriguez <124115753+vraer@users.noreply.github.com>
2023-09-18 16:27:36 -0700

    Update preprocess.py

--- hooks/preprocess.py
+++ hooks/preprocess.py
@@ -1,45 +1,29 @@
 import re
 import glob
-import hashlib
-
-hashes = {}
-
-def file_hash(file_path):
-    with open(file_path, 'rb') as f:
-        return hashlib.md5(f.read()).hexdigest()
 
 def preprocess_markdown(file_path):
-    current_hash = file_hash(file_path)
-    if hashes.get(file_path) == current_hash:
-        return  # Skip unchanged files
-
     with open(file_path, 'r') as f:
         content = f.read()
 
-    content = re.sub(r'<details[^>]*>', '<details markdown>', content)
+    # Flags to check if the file needs updating
+    needs_update = False
 
-    inside_details = False
-    lines = content.split('\\n')
-    modified_lines = []
+    # Check and replace all variations of <details> with <details markdown>
+    new_content, details_count = re.subn(r'<details[^>]*>', '<details markdown>', content)
+    if details_count > 0:
+        needs_update = True
 
-    for line in lines:
-        if '<details' in line:
-            inside_details = True
-        elif '</details>' in line:
-            inside_details = False
+    # Check and remove all <br> tags
+    new_content, br_count = re.subn('<br>', '', new_content)
+    if br_count > 0:
+        needs_update = True
 
-        if inside_details:
-            line = line.replace('<br>', '')
-
-        modified_lines.append(line)
-
-    modified_content = '\\n'.join(modified_lines)
-
-    with open(file_path, 'w') as f:
-        f.write(modified_content)
-
-    hashes[file_path] = current_hash  # Update the hash
+    # Only write back if changes were made
+    if needs_update:
+        with open(file_path, 'w') as f:
+            f.write(new_content)
 
 if __name__ == '__main__':
+    # Locate all Markdown files in current directory and its subfolders
     for markdown_file in glob.glob('**/*.md', recursive=True):
         preprocess_markdown(markdown_file)
